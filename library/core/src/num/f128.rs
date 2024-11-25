1293c9ce200655243e03aa6d8cd03a90623c2b8c
MWDZ <jinjunfeng721@gmail.com>
2024-11-25 15:12:12 -0800

    fix: add placeholder to to_int_uncheck

--- library/core/src/num/f128.rs
+++ library/core/src/num/f128.rs
@@ -885,7 +885,7 @@ impl f128 {
     #[unstable(feature = "f128", issue = "116909")]
     #[must_use = "this returns the result of the operation, without modifying the original"]
     // is_finite() checks if the given float is neither infinite nor NaN.
-    #[requires(self.is_finite() && self >= Self::MIN && self <= Self::MAX)]
+    #[requires(self.is_finite() && /* FIXME */)]
     pub unsafe fn to_int_unchecked<Int>(self) -> Int
     where
         Self: FloatToInt<Int>,
--- library/core/src/num/f16.rs
+++ library/core/src/num/f16.rs
@@ -869,6 +869,7 @@ impl f16 {
     #[inline]
     #[unstable(feature = "f16", issue = "116909")]
     #[must_use = "this returns the result of the operation, without modifying the original"]
+    #[requires(self.is_finite() && /* FIXME */)]
     pub unsafe fn to_int_unchecked<Int>(self) -> Int
     where
         Self: FloatToInt<Int>,
@@ -898,8 +899,6 @@ impl f16 {
     /// # }
     /// ```
     #[inline]
-    #[requires(!self.is_nan() && !self.is_infinite())]
-    #[requires(self >= Self::MIN && self <= Self::MAX)]
     #[unstable(feature = "f16", issue = "116909")]
     #[must_use = "this returns the result of the operation, without modifying the original"]
     pub const fn to_bits(self) -> u16 {
