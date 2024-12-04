7b978eb8260969c3a9328f507ff616e6b60f9d7c
Zyad Hassan <zyadh@amazon.com>
2024-12-03 19:51:38 -0800

    Add -Z float-lib to list command

--- scripts/run-kani.sh
+++ scripts/run-kani.sh
@@ -221,9 +221,8 @@ main() {
             --cbmc-args --object-bits 12
     elif [[ "$run_command" == "list" ]]; then
         echo "Running Kani list command..."
-        "$kani_path" list -Z list -Z function-contracts -Z mem-predicates ./library --std > $path/kani_list.txt
+        "$kani_path" list -Z list -Z function-contracts -Z mem-predicates -Z float-lib ./library --std > $path/kani_list.txt
     fi
 }
 
 main
-
